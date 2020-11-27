d9c4a22f33fd042958e14938075f3842b941034d
Thiago <thiago.nascimento@reclameaqui.com.br>
2020-11-26 22:26:18 -0300

    feat: ensure RemoteAuthentication return an Account on success

--- src/data/usecases/authentication/RemoteAuthentication.ts
+++ src/data/usecases/authentication/RemoteAuthentication.ts
@@ -1,20 +1,20 @@
 import { HttpPostClient } from '@/data/protocols/http/httPostClient'
-import { AuthenticationParams } from '@/domain/usecases/authentication'
+import { AuthenticationParams, Authentication } from '@/domain/usecases/authentication'
 import { HttpStatusCode } from '@/data/protocols/http/httpResponse'
 import { InvalidCredentialsError } from '@/domain/errors/InvalidCredentialsError'
 import { UnexpectedError } from '@/domain/errors/UnexpectedError'
 import { AccountModel } from '@/domain/models/accountModel'
 
-export class RemoteAuthentication {
+export class RemoteAuthentication implements Authentication {
   constructor (
     private readonly url: string,
     private readonly httpPostClient: HttpPostClient<AuthenticationParams, AccountModel>
   ) { }
 
-  async auth (params: AuthenticationParams): Promise<void> {
+  async auth (params: AuthenticationParams): Promise<AccountModel> {
     const httpResponse = await this.httpPostClient.post({ url: this.url, body: params })
     switch (httpResponse.statusCode) {
-      case HttpStatusCode.ok: break
+      case HttpStatusCode.ok: return httpResponse.body
       case HttpStatusCode.unauthorized: throw new InvalidCredentialsError()
       default: throw new UnexpectedError()
     }
