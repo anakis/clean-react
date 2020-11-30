9f06fdfee55e5f7f59dbb850a55c061594948b47
Thiago <thiago.nascimento@reclameaqui.com.br>
2020-11-30 15:55:48 -0300

    feat: add login view

---
+++ src/presentation/components/spinner/spinner.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from 'react'
+import Styles from './spinner.scss'
+
+type Props = React.HTMLAttributes<HTMLDivElement>
+
+const Spinner = (props: Props): ReactElement => {
+  return (
+    <div {...props} className={[Styles.spinner, props.className].join(' ')}><div></div><div></div><div></div><div></div></div>
+  )
+}
+
+export default Spinner
