aad9e6d40cbbb5625783ee370fb3385ee00c96b6
Thiago <thiago.nascimento@reclameaqui.com.br>
2020-12-14 18:29:35 -0300

    feat: ensure Login present error if password is invalid

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -20,13 +20,10 @@ const Login = ({ validation }: Props): React.ReactElement => {
   useEffect(() => {
     setState({
       ...state,
-      emailError: validation.validate('email', state.email)
+      emailError: validation.validate('email', state.email),
+      passwordError: validation.validate('password', state.password)
     })
-  }, [state.email])
-
-  useEffect(() => {
-    validation.validate('password', state.password)
-  }, [state.password])
+  }, [state.email, state.password])
 
   return (
     <div className={Styles.login}>
