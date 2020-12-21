ebd70612d7fa7ac2196b9719a826539048957667
Thiago <thiago.nascimento@reclameaqui.com.br>
2020-12-21 15:30:08 -0300

    fix: small fix on requredFieldValidation imports

--- src/validation/validators/requiredField/requiredFieldValidation.spec.ts
+++ src/validation/validators/requiredField/requiredFieldValidation.spec.ts
@@ -1,4 +1,4 @@
-import { RequiredFieldValidation } from '@/validation/validators/requiredField'
+import { RequiredFieldValidation } from '@/validation/validators'
 import { RequiredFieldError } from '@/validation/errors'
 import faker from 'faker'
 
