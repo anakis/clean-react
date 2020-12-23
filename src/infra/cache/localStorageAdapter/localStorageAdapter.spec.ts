b1005ed8bd0e22f0a354d8fd42271874dac1a92b
Thiago <thiago.nascimento@reclameaqui.com.br>
2020-12-23 12:13:09 -0300

    refactor: move SUT creation to a factory method

--- src/infra/cache/localStorageAdapter/localStorageAdapter.spec.ts
+++ src/infra/cache/localStorageAdapter/localStorageAdapter.spec.ts
@@ -2,11 +2,13 @@ import faker from 'faker'
 import 'jest-localstorage-mock'
 import { LocalStorageAdapter } from '@/infra/cache/localStorageAdapter/localStorageAdapter'
 
+const makeSut = () => new LocalStorageAdapter()
+
 describe('LocalStorageAdapter', () => {
   beforeEach(() => localStorage.clear())
 
   it('Should call localStorage with correct values', async () => {
-    const sut = new LocalStorageAdapter()
+    const sut = makeSut()
     const key = faker.database.column()
     const value = faker.random.word()
     await sut.set(key, value)
