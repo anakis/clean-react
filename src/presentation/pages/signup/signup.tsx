819a48897a47878f24762ce553cf5247fb389a8e
Thiago <thiago.nascimento@reclameaqui.com.br>
2021-02-24 21:06:30 -0300

    feat: ensure SignUp starts with correct initial state

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -1,27 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Styles from './signup.scss'
 import { Footer, LoginHeader, Input, FormStatus } from '@/presentation/components'
 import Context from '@/presentation/contexts/form/formContext'
 import { Link } from 'react-router-dom'
 
-const SignUp = (): React.ReactElement => (
-  <div className={Styles.signup}>
-    <LoginHeader />
-    <Context.Provider value={{ state: {} }}>
-      <form className={Styles.form}>
-        <h2>Criar conta</h2>
-        <Input type="text" name="text" placeholder="Digite seu nome" />
-        <Input type="email" name="email" placeholder="Digite seu e-mail" />
-        <Input type="password" name="password" placeholder="Digite sua senha" />
-        <Input type="password" name="passwordConfirmation" placeholder="Repita sua senha" />
-        <button className={Styles.submit} type="submit">Salvar</button>
-        <Link to="/login" className={Styles.link}>Voltar para Login</Link>
-        <FormStatus />
-      </form>
+const SignUp = (): React.ReactElement => {
+  const [state] = useState({
+    isLoading: false,
+    nameError: 'Campo obrigatório',
+    emailError: 'Campo obrigatório',
+    passwordError: 'Campo obrigatório',
+    passwordConfirmationError: 'Campo obrigatório',
+    mainError: ''
+  })
+  return (
+    <div className={Styles.signup}>
+      <LoginHeader />
+      <Context.Provider value={{ state }}>
+        <form className={Styles.form}>
+          <h2>Criar conta</h2>
+          <Input type="text" name="name" placeholder="Digite seu nome" />
+          <Input type="email" name="email" placeholder="Digite seu e-mail" />
+          <Input type="password" name="password" placeholder="Digite sua senha" />
+          <Input type="password" name="passwordConfirmation" placeholder="Repita sua senha" />
+          <button data-testid="submit" disabled className={Styles.submit} type="submit">Salvar</button>
+          <Link to="/login" className={Styles.link}>Voltar para Login</Link>
+          <FormStatus />
+        </form>
 
-    </Context.Provider>
-    <Footer />
-  </div>
-)
+      </Context.Provider>
+      <Footer />
+    </div>
+  )
+}
 
 export default SignUp
