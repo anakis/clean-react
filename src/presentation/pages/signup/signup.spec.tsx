ea57972d375e7fefc130c7e39475e90e7f508414
Thiago Nascimento <thiag.g3@gmail.com>
2021-04-01 18:27:45 -0300

    test: ensure signup prevent submit if form is invalid

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -167,4 +167,13 @@ describe('Signup', () => {
 
     expect(addAccountSpy.callsCount).toBe(1)
   })
+
+  it('Should not call AddAccount if form is invalid', async () => {
+    const validationError = faker.random.words()
+    const { sut, addAccountSpy } = makeSut({ validationError })
+
+    await simulateValidSubmit(sut)
+
+    expect(addAccountSpy.callsCount).toBe(0)
+  })
 })
